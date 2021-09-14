ecf47d05ce302631012d3c24a2cb7a1e1e9fa026
3n3a <info@3n3a.ch>
2021-09-14 09:44:18 +0200

    added script for latex generation from md

---
+++ eisvogel_latex_template.sh
@@ -0,0 +1,7 @@
+
+# Download the template for linux
+mkdir /usr/share/pandoc/data/templates/eisvogel
+curl -fsSL -o /usr/share/pandoc/data/templates/eisvogel/eisvogel.latex https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex
+
+# Command to use with Markdown
+# pandoc --template=eisvogel/eisvogel.latex README.md --pdf-engine=xelatex --toc -o Zusammenfassung_M105.pdf

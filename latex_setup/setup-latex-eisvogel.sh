024dd9a2d72c40217934a8a8f4e92e0dced887b7
3n3a <46775561+3n3a@users.noreply.github.com>
2021-09-14 10:03:22 +0200

    Update setup-latex-eisvogel.sh

--- latex_setup/setup-latex-eisvogel.sh
+++ latex_setup/setup-latex-eisvogel.sh
@@ -4,4 +4,4 @@ mkdir /usr/share/pandoc/data/templates/eisvogel
 curl -fsSL -o /usr/share/pandoc/data/templates/eisvogel/eisvogel.latex https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex
 
 # Command to use with Markdown
-# pandoc --template=eisvogel/eisvogel.latex README.md --pdf-engine=xelatex --toc -o Zusammenfassung_M105.pdf
+# pandoc --template=eisvogel/eisvogel.latex README.md --pdf-engine=xelatex --toc -o Zusammenfassung_M105.pdf --listings

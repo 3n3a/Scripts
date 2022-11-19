306b5358ecd8bf5e1d080d109fba2fc3758ff0e7
3n3a <46775561+3n3a@users.noreply.github.com>
2022-11-19 14:21:41 +0100

    doesnt need python

--- internet/connectivity.sh
+++ internet/connectivity.sh
@@ -3,12 +3,11 @@
 # Am I connected?
 #
 # NEEDS:
-#    python3
 #    curl
 
-connected=$(python3 -c "import sys; print('$(curl -s http://www.msftconnecttest.com/connecttest.txt)' == 'Microsoft Connect Test')")
+CONN=$(curl -s http://www.msftconnecttest.com/connecttest.txt)
 
-if [[ "$connected" -eq "True" ]]; then
+if [ "$CONN" = "Microsoft Connect Test" ]; then
   echo "You are connected to the Internet"
   exit 0
 else

0c3a1a5bf753f3d2a1e18a4ea4ebc14e4dcb8a21
3n3a <46775561+3n3a@users.noreply.github.com>
2022-11-19 14:27:33 +0100

    Update connectivity.sh

--- internet/connectivity.sh
+++ internet/connectivity.sh
@@ -6,9 +6,11 @@
 #    curl
 
 CONN=$(curl -s http://www.msftconnecttest.com/connecttest.txt)
+PING=$(ping -c 1 www.msftconnecttest.com | grep time= | sed -E 's/.*time=//gm;t;d')
 
 if [ "$CONN" = "Microsoft Connect Test" ]; then
   echo "You are connected to the Internet"
+  echo "PING: $PING"
   exit 0
 else
   echo "Your are offline"

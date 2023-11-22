8aeeac497dc60f4eec7ea3db7b82cd9d09589f9a
3n3a <46775561+3n3a@users.noreply.github.com>
2023-11-22 07:21:31 +0100

    Update install-golang.sh

--- installer-scripts/install-golang.sh
+++ installer-scripts/install-golang.sh
@@ -1,5 +1,12 @@
 #install go
 
+arch=$(uname -m)
+go_arch="amd64"
+
+if [ arch -eq "armv7l" ]; then
+  go_arch="armv6l"
+fi
+
 mkdir -p /tmp/go-install
 cd /tmp/go-install
 curl -fsSL --output go.tar.gz "https://go.dev/dl/$(curl "https://go.dev/VERSION?m=text").linux-amd64.tar.gz"

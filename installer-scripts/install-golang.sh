6e84a087c3effb09920a890d709f00fa2c5faa2e
3n3a <46775561+3n3a@users.noreply.github.com>
2023-11-22 07:22:48 +0100

    Update install-golang.sh

--- installer-scripts/install-golang.sh
+++ installer-scripts/install-golang.sh
@@ -9,7 +9,7 @@ fi
 
 mkdir -p /tmp/go-install
 cd /tmp/go-install
-curl -fsSL --output go.tar.gz "https://go.dev/dl/$(curl "https://go.dev/VERSION?m=text").linux-amd64.tar.gz"
+curl -fsSL --output go.tar.gz "https://go.dev/dl/$(curl "https://go.dev/VERSION?m=text").linux-$go_arch.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local/ -xzf go.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee -a /etc/profile

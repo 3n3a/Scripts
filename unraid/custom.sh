53b60e194f7c342c7a16675a9485aa32e53e1910
3n3a <46775561+3n3a@users.noreply.github.com>
2024-01-26 17:48:46 +0100

    Update custom.sh

---
+++ unraid/custom.sh
@@ -0,0 +1,10 @@
+## belongs into
+## /etc/profile.d/custom.sh
+
+#!/bin/bash
+#
+# custom by 3n3a
+
+alias e="cd /mnt/user/EneaDrive"
+alias ll="ls -lha"
+export PATH="$PATH:/mnt/user/EneaDrive/Scripts"
